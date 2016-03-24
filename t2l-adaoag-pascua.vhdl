643feb6df0e1bf87a06692e395f30a7af1988327
Rokuto <gauven.boro@gmail.com>
2016-03-24 01:19:10 -0400

    Update and rename st8l-adaoag-pascua.vhdl to t2l-adaoag-pascua.vhdl

--- t2l-adaoag-pascua.vhdl
+++ t2l-adaoag-pascua.vhdl
@@ -2,11 +2,11 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity st8l_adaoag_pascua is
+entity t2l_adaoag_pascua is
 	port( in_boggis, in_bunce, in_bean, out_boggis, out_bunce, out_bean : in std_logic; alarm: out std_logic );
-end st8l_adaoag_pascua;
+end t2l_adaoag_pascua;
 
-architecture buzzer of st8l_adaoag_pascua is
+architecture buzzer of t2l_adaoag_pascua is
 	begin
 		alarm <= ( in_boggis or in_bunce or in_bean ) and ( out_boggis or out_bunce or out_bean );
 end architecture buzzer; 
